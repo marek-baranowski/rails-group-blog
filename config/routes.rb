dfe3fda893f7540b5fc29ac2b754f51cc569d3e4
Marek Baranowski <mbrnwsk@gmail.com>
2016-03-06 04:47:27 +0100

    Add more meaningful route name for login

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   resources :user_sessions
 
   get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => 'user_sessions#create'
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
 =begin
