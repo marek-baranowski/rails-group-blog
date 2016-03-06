eb69aeddc6f4b8b7912a93fdcfaae17981ebeb15
Marek Baranowski <mbrnwsk@gmail.com>
2016-03-06 01:00:08 +0100

    Set proper redirection on authorization violation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :require_login
+
+  private
+  def not_authenticated
+    flash[:alert] = 'You have to authenticate to access this page.'
+    redirect_back_or_default
+  end
+
+  def redirect_back_or_default(default = root_url)
+    redirect_to :back
+  rescue ActionController::RedirectBackError
+    redirect_to default
+  end
 end
