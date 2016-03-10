b30133ca6f4132eb75388ca11058573a75426516
Marek Baranowski <mbrnwsk@gmail.com>
2016-03-10 09:23:18 +0100

    Improve validation and displaying error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ActiveRecord::Base
   enum status: [:contributor, :admin]
 
   authenticates_with_sorcery!
-
+  validates :email, uniqueness: true, presence: true
   validates :password, length: {minimum: 3}, if: -> { new_record? || changes["password"] }
   validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
 
-  validates :email, uniqueness: true
 end
