7bd10c20ba0fa04107d908838bd730bb779aadec
Marek Baranowski <mbrnwsk@gmail.com>
2016-03-10 11:03:51 +0100

    Code cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,63 +3,47 @@ class UsersController < ApplicationController
   before_action :admin_only, only: [:destroy]
   before_action :owns_resource, only: [:show, :edit, :update]
   skip_before_action :require_login, only: [:new, :create, :show]
-  # GET /users
-  # GET /users.json
+
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
         format.html { redirect_to login_path, notice: 'User was successfully created. Now you have to wait for activation.' }
-        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -67,17 +51,14 @@ class UsersController < ApplicationController
     @user.activate!
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully activated.' }
-      format.json { head :no_content }
     end
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
